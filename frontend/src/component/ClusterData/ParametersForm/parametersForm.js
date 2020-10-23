474d34dffecec4bfd1f4bf352e010ef7659b11fa
Konstantin Bogdanoski <konstantin.b@live.com>
2020-10-23 13:14:24 +0200

    Update requirements & Refactor UI
    Signed-off-by: Konstantin Bogdanoski <konstantin.b@live.com>

--- frontend/src/component/ClusterData/ParametersForm/parametersForm.js
+++ frontend/src/component/ClusterData/ParametersForm/parametersForm.js
@@ -17,7 +17,7 @@ const ParametersForm = (props) => {
                            className="form-control border-info"
                            placeholder="Absolute path to blanket clusterer (main.py in the module)"
                            aria-label="BlanketClusterer path"
-                           aria-describedby="basic-addon1" required={true} onChange={props.handleInputChange}
+                           aria-describedby="basic-addon1" required onChange={props.handleInputChange}
                            value={props.data.blanketClustererPath != null ? props.data.blanketClustererPath : ""}/>
                 </div>
             </div>
@@ -30,7 +30,7 @@ const ParametersForm = (props) => {
                     <input type="text" name="outputPath"
                            className="form-control border-info"
                            placeholder="Absolute path to output file" aria-label="Output path"
-                           aria-describedby="basic-addon1" required={true} onChange={props.handleInputChange}
+                           aria-describedby="basic-addon1" required onChange={props.handleInputChange}
                            value={props.data.outputPath != null ? props.data.outputPath : ""}/>
                 </div>
             </div>
@@ -42,8 +42,12 @@ const ParametersForm = (props) => {
                     </div>
                     <select className="form-control border-info"
                             name="modelType"
+                            required
                             onChange={props.handleInputChange}
                     >
+                        <option value="">
+                            Please select an algorithm
+                        </option>
                         <option value="k-means">
                             K-Means
                         </option>
@@ -69,7 +73,7 @@ const ParametersForm = (props) => {
                            className="form-control border-info"
                            placeholder="How many clusters?"
                            aria-label="Number of clusters"
-                           aria-describedby="basic-addon1" required={true} onChange={props.handleInputChange}
+                           aria-describedby="basic-addon1" required onChange={props.handleInputChange}
                            value={props.data.numClusters != null ? props.data.numClusters : ""}/>
                 </div>
             </div>
@@ -83,7 +87,7 @@ const ParametersForm = (props) => {
                            className="form-control border-info"
                            placeholder="How many kids should the clusters tend to have?"
                            aria-label="Number of items in clusters"
-                           aria-describedby="basic-addon1" required={true} onChange={props.handleInputChange}
+                           aria-describedby="basic-addon1" required onChange={props.handleInputChange}
                            value={props.data.numItems != null ? props.data.numItems : ""}/>
                 </div>
             </div>
@@ -96,7 +100,7 @@ const ParametersForm = (props) => {
                     <input type="number" name="maximumDepth" min={1} max={6}
                            className="form-control border-info"
                            placeholder="How deep do you want to go?" aria-label="Maximum depth"
-                           aria-describedby="basic-addon1" required={true} onChange={props.handleInputChange}
+                           aria-describedby="basic-addon1" required onChange={props.handleInputChange}
                            value={props.data.maximumDepth != null ? props.data.maximumDepth : ""}/>
                 </div>
             </div>
